d4c1db2b9d6f2b2899c0f7761e947890540ac982
kevinhsu <kevin19950820@gmail.com>
2021-10-27 15:47:00 +0800

    write restful

--- dockerfile.dockerfile
+++ dockerfile.dockerfile
@@ -2,7 +2,6 @@ FROM ubuntu:18.04
 
 MAINTAINER kevin
 
-RUN apt-get update -y
 ADD restful.py
 
 EXPOSE 5100
--- restful.py
+++ restful.py
@@ -1,1 +1,11 @@
-print('v1')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return 'v1'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
